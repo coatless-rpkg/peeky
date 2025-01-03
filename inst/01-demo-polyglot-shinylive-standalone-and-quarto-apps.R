8b8f8639182159f94d3527abafbdfb6d223d8db6
james.balamuta@gmail.com <james.balamuta@gmail.com>
2025-01-02 23:58:18 -0800

    Add recorded GIF demo URLs

---
+++ inst/01-demo-polyglot-shinylive-standalone-and-quarto-apps.R
@@ -0,0 +1,10 @@
+# Embedded Python Shinylive Applications in a Quarto Document
+peeky::peek_shinylive_app(
+    "https://quarto-ext.github.io/shinylive",
+    output_dir = "quarto-shiny-apps")
+
+# Standalone R Shinylive Application
+peeky::peek_shinylive_app(
+    "https://tutorials.thecoatlessprofessor.com/convert-shiny-app-r-shinylive/",
+    output_dir = "r-shiny-app")
+
---
+++ inst/02-demo-complex-apps.R
@@ -0,0 +1,10 @@
+# Module/Script file heavy
+peeky::peek_shinylive_app(
+    "https://shiny.thecoatlessprofessor.com/probability-distribution-explorer/",
+    output_dir = "probability-distribution-explorer")
+
+# Resource rich
+peeky::peek_shinylive_app(
+    "https://jeanjoe.net/peeky_example/",
+    output_dir = "peeky-example-data")
+
